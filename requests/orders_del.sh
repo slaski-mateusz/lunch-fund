7d2e29d2fd13edeac9850396e19c6ba613924279
Mateusz Ślaski <slaski.mateusz@gmail.com>
2022-10-17 22:00:01 +0200

    scripts to test orders update and del

---
+++ requests/orders_del.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+source ./functions.sh
+
+parseOptions "$@"
+
+BODY_TEMPLATE=`loadBodyTemplate`
+
+BODY=$( jq -n \
+--arg teamname "$teamname" \
+--argjson id $id \
+"$BODY_TEMPLATE"\
+)
+
+echo $BODY
+
+curl -v -X DELETE -d "$BODY" "127.0.0.1:8080/api/members/"
---
+++ requests/orders_update.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+source ./functions.sh
+
+parseOptions "$@"
+
+BODY_TEMPLATE=`loadBodyTemplate`
+
+BODY=$( jq -n \
+--arg teamname "$teamname" \
+--argjson id "$id" \
+--arg ordername "$ordername" \
+--argjson timestamp "$timestamp" \
+--argjson founderid "$founderid" \
+--argjson deliverycost "$deliverycost" \
+--argjson tipcost "$tipcost" \
+"$BODY_TEMPLATE"\
+)
+
+echo $BODY
+
+curl -v -X PUT -d "$BODY" "127.0.0.1:8080/api/teams/"
+curl -v -X POST -d "$BODY" 127.0.0.1:8080/members/
