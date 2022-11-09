9fb195abcb60f4d7ee5f8b124c79d1c70fe4270f
Mateusz Ślaski <slaski.mateusz@gmail.com>
2022-11-09 23:18:46 +0100

    wip

--- backend/api/orders-details.go
+++ backend/api/orders-details.go
@@ -1,15 +1,36 @@
 package api
 
 import (
-	// "encoding/json"
+	"encoding/json"
 	"net/http"
-	// "github.com/slaski-mateusz/lunch-fund/backend/db"
+
+	"github.com/slaski-mateusz/lunch-fund/backend/db"
+	"github.com/slaski-mateusz/lunch-fund/backend/model"
 	// "github.com/slaski-mateusz/lunch-fund/backend/model"
 )
 
-func ordersDetailsHandler(reswri http.ResponseWriter, requ *http.Request) {
+func ordersDetailsHandler(resWri http.ResponseWriter, requ *http.Request) {
 	switch requ.Method {
 	case http.MethodGet:
+		var orderDetailsRequest model.Order
+		errDecode := json.NewDecoder(requ.Body).Decode(&orderDetailsRequest)
+		if errDecode != nil {
+			http.Error(resWri, errDecode.Error(), http.StatusBadRequest)
+		}
+		if orderDetailsRequest.TeamName == "" {
+			http.Error(resWri, "Team name can not to be emty!", http.StatusBadRequest)
+			return
+		}
+		orders, errOrd := db.ListOrdersDetails(
+			orderDetailsRequest.TeamName,
+			orderDetailsRequest.Id,
+		)
+		if errOrd != nil {
+			http.Error(resWri, errOrd.Error(), http.StatusBadRequest)
+			return
+		}
+		json.NewEncoder(resWri).Encode(orders)
+		return
 	case http.MethodPut:
 	case http.MethodPost:
 	case http.MethodDelete:
---
+++ requests/orders_details_list.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+source ./functions.sh
+
+parseOptions "$@"
+
+BODY_TEMPLATE=`loadBodyTemplate`
+
+BODY=$( jq -n \
+--arg teamname "$teamname" \
+--argjson id "$id"
+"$BODY_TEMPLATE"\
+)
+
+# BODY=`echo $BODY_TEMPLATE | tr '\n' ' ' | sed s/\{\{TEAM_NAME\}\}/"$TEAM_NAME"/g `
+echo $BODY
+
+# curl -v -X GET -d "$BODY" "127.0.0.1:8080/api/orders/"
+curl -v -X GET -d "$BODY" "127.0.0.1:8080/api/orders/" | jq .
